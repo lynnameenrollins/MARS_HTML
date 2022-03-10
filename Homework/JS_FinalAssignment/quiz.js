b1c7d26ef77c94b62f337972567dd297d6c1b7f0
lynnameenrollins <lynnameenrollins@gmail.com>
2022-03-10 06:48:09 -0500

    Final JS homework assignment

--- Homework/JS_FinalAssignment/quiz.js
+++ Homework/JS_FinalAssignment/quiz.js
@@ -1,20 +1,30 @@
 
-
-function checkAnswer(){
-
-    console.log("Here in checkAnswer")
-    //when here check if the answer is correct
-    //change colors to show if correct(green)/incorrect (red)
+function wrongAnswer(clicked_id){
+    console.log("Here in wrongAnswer")
+    let btn = document.getElementById(clicked_id)
+    btn.style.backgroundColor = '#bf2e3f';
+       
     //enable Next button 
-    
     document.getElementById("next").disabled = false;
-    
-    
+        
+}
+function correctAnswer(clicked_id){
+    console.log("Here in correctAnswer")
+    let btn = document.getElementById(clicked_id)
+    btn.style.backgroundColor = '#4ead4c';
+       
+    //enable Next button 
+    document.getElementById("next").disabled = false;
+        
 }
 
 function getNextQuestion(){
     console.log("Here in get Next Question")
     //when in here look up the next question
+    b1.style.backgroundColor = '#d4cfd0';
+    b2.style.backgroundColor = '#d4cfd0';
+    b3.style.backgroundColor = '#d4cfd0';
+    b4.style.backgroundColor = '#d4cfd0';
     loadQandA()
 
     //then need to disable the next button again
@@ -23,6 +33,107 @@ function getNextQuestion(){
 
 function loadQandA(){
     //loads the next question from a list of questions
-    var Question = getElementById("question") 
-    Question = "I will load the question now in this function"
+
+
+    let {questionNo, question, a1, a2, a3, correct} = questionBank.pop()
+    dispQuestion.innerHTML = question
+    //randomize correct answer position
+    var correctAnswerPosition = Math.floor(Math.random()*4) + 1
+
+            b1.innerHTML = a1;
+            b2.innerHTML = a2;
+            b3.innerHTML = a3;
+            b4.innerHTML = correct;
+              
 }
+
+class QuestionBank{
+    #questionNo;
+    #question;
+    #a1;
+    #a2;
+    #a3;
+    #correct;
+    constructor(questionNo, question, a1, a2, a3, correct){
+        this.#questionNo = questionNo;
+        this.#question = question;
+        this.#a1 = a1;
+        this.#a2 = a2;
+        this.#a3 =a3;
+        this.#correct = correct
+    }
+
+    get questionNo() {
+        return this.#questionNo
+    }
+    set questionNo(questionNo){
+    
+        this.#questionNo = questionNo;
+    }
+    get question() {
+        return this.#question
+    }
+    set question(question){
+        
+        this.#question = question;
+    }
+    get a1() {
+        return this.#a1
+    }
+    set a1(a1){
+        
+        this.#a1 = a1;
+    }
+    get a2() {
+        return this.#a2
+    }
+    set a2(a2){
+        
+        this.#a2 = a2;
+    }
+    get a3() {
+        return this.#a3
+    }
+    set a3(sa3){
+        
+        this.#a3 = a3;
+    }
+    get correct() {
+        return this.#correct
+    }
+    set correct(correct){
+        
+        this.#correct = correct;
+    }
+    
+    
+}
+
+    let questionBank = [
+        { questionNo: 1, question: 'What is the correct way to write an If Statement in JavaScript', 
+         a1: "if i=5 then", a2: "if i = 5", a3: "if i==5 then", correct: "if (i== 5)" },
+         { questionNo: 2, question: 'There are _____ level of heading in html', 
+         a1: "three", a2: "four", a3: "five", correct: "six" },
+         { questionNo: 3, question: 'How do you make each word in a text start with a capital letter?', 
+         a1: "text-style:capitalize", a2: "transform:capitalize", a3: "You can't do that with CSS", correct: "text-transform:capitalize  " },
+         { questionNo: 3, question: 'What does CSS stand for?', 
+         a1: "Creative Style Sheets", a2: "Colorful Style Sheets", a3: "Computer Style Sheets", correct: "Cascading Style Sheet" },
+         { questionNo: 5, question: 'Which CSS property is used to change the text color of an element?', 
+         a1: "background-color", a2: "bg-color", a3: "fgcolor", correct: "color" }
+        
+    
+    ]    
+    
+    //loads the first question
+    var dispQuestion = document.getElementById("questionText") 
+    var b1 = document.getElementById("1") 
+    var b2 = document.getElementById("2") 
+    var b3 = document.getElementById("3") 
+    var b4 = document.getElementById("4") 
+    loadQandA()
+    console.log(questionBank)
+    
+    
+    
+    
+  
